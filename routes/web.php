4b13fa49ab7ae690efa7d91969bfd4f552c1d6ce
risazahrani404 <risazahrani404@gmail.com>
2024-07-05 13:49:21 +0700

    tambah halaman dashboard, data penjualan

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,16 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('tes', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
+});
+Route::get('/input-transaksi', function () {
+    return view('data-penjualan/input-transaksi');
+});
+Route::get('/riwayat-transaksi', function () {
+    return view('data-penjualan/riwayat-transaksi');
+});
+
+Route::get('/input-obat', function () {
+    return view('obat/input');
 });
